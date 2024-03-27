5da50ae6b5966843c67bd2d4034dc94e086d3afe
sunqinjian <884829003@qq.com>
2024-03-27 18:35:08 +0800

    update

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,7 @@
 export const API_CONFIG  = {
     loginApi: "https://mock.mengxuegu.com/mock/65cf63a8351bbd02cf3398ab/api/auth/login",
-    logoutApi: "https://mock.mengxuegu.com/mock/65cf63a8351bbd02cf3398ab/api/auth/logout"
+    logoutApi: "https://mock.mengxuegu.com/mock/65cf63a8351bbd02cf3398ab/api/auth/logout",
+    userListApi: "https://mock.mengxuegu.com/mock/65d498da351bbd02cf339bad/vueapi/user/list"
 }
 
 export const CONFIG = {
--- src/views/user/List.vue
+++ src/views/user/List.vue
@@ -1,32 +1,60 @@
 <script  setup>
-const tableData = [
-  {
-    date: '2016-05-03',
-    name: 'Tom',
-    address: 'No. 189, Grove St, Los Angeles',
-  },
-  {
-    date: '2016-05-02',
-    name: 'Tom',
-    address: 'No. 189, Grove St, Los Angeles',
-  },
-  {
-    date: '2016-05-04',
-    name: 'Tom',
-    address: 'No. 189, Grove St, Los Angeles',
-  },
-  {
-    date: '2016-05-01',
-    name: 'Tom',
-    address: 'No. 189, Grove St, Los Angeles',
-  },
-]
+import {getUserList} from '../../api/user.js';
+import { ElMessage } from 'element-plus';
+import {reactive,toRefs} from 'vue'
+
+const userList = reactive({
+  data: ""
+})
+//获取用户列表
+const getUser = () =>{
+  getUserList()
+    .then((response) => {
+      // console.log("获取成功，response:",response)
+      // response.status http请求的状态,axios自动返回的
+      // response.data.status 后端返回的状态，需要后端提供data.status
+      if ( response.data.status == 200 ){
+        // 获取成功提示
+        ElMessage({
+          message: response.data.message,
+          type: 'success',
+       })
+      // const userList = [response.data.data.items][0]
+      userList.data = response.data.data.items
+      console.log("用户信息",userList)
+      }
+    })
+     //.catch错误在守卫中处理
+  
+  }
+  const {userList2} = toRefs(userList.data)
 </script>
 
 <template>
-  <el-table :data="tableData" style="width: 100%">
-    <el-table-column prop="date" label="Date" width="180" />
-    <el-table-column prop="name" label="Name" width="180" />
-    <el-table-column prop="address" label="Address" />
-  </el-table>
+
+
+  <el-card >
+    <template #header>
+      <div class="card-header">
+        <span>用户列表</span>
+        <el-button text bg @click="getUser()">获取用户列表</el-button>
+      </div>
+    </template>
+
+      <el-table :data="userList2" style="width: 100%">
+        <!-- <el-table v-for="(userinfo) in userList" :key="userinfo.id" :data="userinfo" style="width: 100%">  -->
+        <el-table-column prop="id" label="Id" width="180" />
+        <el-table-column prop="username" label="Name" width="180" />
+        <el-table-column prop="qq" label="QQ" width="180" />
+        <el-table-column prop="address" label="Address" />
+      </el-table>
+  </el-card>
 </template>
+
+<style>
+.card-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+</style>
